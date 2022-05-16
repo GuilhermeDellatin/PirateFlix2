553c3c535eafe0bdedd5c59b2d7d5441881d00f3
GuilhermeDellatin <guidellatin@gmail.com>
2022-05-16 15:29:46 -0300

    Create adapter and adjust main

---
+++ app/src/main/java/com/gfdellatin/pirateflix/MoviesAdapter.kt
@@ -0,0 +1,42 @@
+package com.gfdellatin.pirateflix
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import coil.load
+import androidx.recyclerview.widget.RecyclerView
+import com.gfdellatin.pirateflix.remote.MovieResponse
+
+private const val basePosterUrl = "https://image.tmdb.org/t/p/w500/"
+
+class MoviesAdapter(
+    private val movies: List<MovieResponse>
+) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.movie_item, parent, false)
+
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+
+        holder.apply {
+            movieName.text = movie.title
+            poster.load(
+                basePosterUrl + movie.posterPath
+            )
+        }
+    }
+
+    override fun getItemCount() = movies.size
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val movieName: TextView = itemView.findViewById(R.id.movie_name)
+        val poster: ImageView = itemView.findViewById(R.id.poster)
+    }
+}

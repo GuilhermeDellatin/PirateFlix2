f6627311be2845e498b7691338a7cea3969121f5
GuilhermeDellatin <guidellatin@gmail.com>
2022-05-16 15:28:46 -0300

    Adjust dependencies

---
+++ app/src/main/java/com/gfdellatin/pirateflix/MainFragment.kt
@@ -0,0 +1,61 @@
+package com.gfdellatin.pirateflix
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.gfdellatin.pirateflix.remote.BaseResponse
+import com.gfdellatin.pirateflix.remote.MovieResponse
+import com.gfdellatin.pirateflix.remote.ServiceProvider
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainFragment : Fragment() {
+
+    private lateinit var movies: RecyclerView
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        movies = view.findViewById(R.id.movies)
+
+        getMovies()
+    }
+
+    private fun getMovies() {
+        ServiceProvider.service.getMovies(BuildConfig.tmdbToken)
+            .enqueue(object : Callback<BaseResponse<List<MovieResponse>>> {
+                override fun onResponse(
+                    call: Call<BaseResponse<List<MovieResponse>>>,
+                    response: Response<BaseResponse<List<MovieResponse>>>
+                ) {
+                    if (response.isSuccessful) {
+                        val data = response.body()?.results
+                        movies.adapter = MoviesAdapter(data ?: emptyList())
+                    }
+                }
+
+                override fun onFailure(
+                    call: Call<BaseResponse<List<MovieResponse>>>,
+                    t: Throwable
+                ) {
+
+                }
+
+            })
+    }
+}

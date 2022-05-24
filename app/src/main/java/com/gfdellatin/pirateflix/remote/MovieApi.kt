6e6acbb75ed41f32eeb8ce9117a6ea5b1a36ed03
GuilhermeDellatin <guidellatin@gmail.com>
2022-05-24 17:18:53 -0300

    Adjust

--- app/src/main/java/com/gfdellatin/pirateflix/remote/MovieApi.kt
+++ app/src/main/java/com/gfdellatin/pirateflix/remote/MovieApi.kt
@@ -1,10 +1,12 @@
 package com.gfdellatin.pirateflix.remote
 
+import com.gfdellatin.pirateflix.data.model.BasePaginationRemote
+import com.gfdellatin.pirateflix.data.model.MovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieApi {
 
     @GET("/discover/movie")
-    suspend fun getMovies(@Query("api_key") tmdbToken: String) : BaseResponse<List<MovieResponse>>
+    suspend fun getMovies(@Query("api_key") tmdbToken: String) : BasePaginationRemote<List<MovieResponse>>
 }
